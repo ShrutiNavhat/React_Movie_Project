f57d7e0a3562f374970a848bf203674b722ce42c
Shruti Navhat <shrutinavhat21@navgurukul.org>
2023-07-02 16:05:00 +0530

    complete Header

---
+++ app/Header.js
@@ -0,0 +1,36 @@
+function Header () {
+    return (
+        <> 
+            <div className="space"></div>
+            <div className="space1">
+                <div className="Title_row">
+                    <div className="Icon_logo">
+                        <img src="logo.png" class="logo"/>
+                    </div>
+                    <div className="Movie_hub">
+                         <p class="ac"><b>MOVIE HUB</b></p>
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="Watch">
+                        <p class="text">Watch</p>
+                    </div>
+                    <div className="Add_movie">
+                        <p class="text">Add Movie</p>
+                    </div>
+                </div>
+            </div>
+            <div className="space6"></div>
+            <div className="space2"></div>
+            <div className="space3"></div>
+            <div className="space4">
+                <div className="Watch_text"></div>
+                 <p class="text1">Watch</p>
+            </div>
+            <div className="space5"></div>
+            
+        </>
+    )
+
+}
+export default Header;
---
+++ app/Movie_row.js
@@ -0,0 +1,22 @@
+
+
+const Movie_row =()=>{
+    return (
+        <div className="row data_row">
+            <div className="image container">
+                <img src=""/>
+            </div>
+            <div className="movie_detail">
+                <h2>Dear Zindagi</h2>
+                <p></p>
+                <h3>Description</h3>
+                <p></p>
+            </div>
+        </div>
+            
+            
+        
+            
+    )
+}
+export default  Movie_row;
